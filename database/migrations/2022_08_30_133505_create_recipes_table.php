d381e05b0dc7068aecb1adbf731e37064abca423
hvtk <hvtk1968@gmail.com>
2022-08-30 20:43:34 +0200

    added recipes database

---
+++ database/migrations/2022_08_30_133505_create_recipes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string('recipe_title');
+            $table->string('recipe_section');
+            $table->string('kitchenware');
+            $table->string('ingredients');
+            $table->string('recipe_the_steps_to_follow');
+            $table->string('recipe_estimated_time');
+            $table->string('recipe_image_end_result');
+            $table->string('recipe_shelf_life');
+            $table->longText('recipe_commentary');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
