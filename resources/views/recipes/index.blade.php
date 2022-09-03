3bc118aaf83bbfcea29f832565762ebf6d9daefa
hvtk <hvtk1968@gmail.com>
2022-09-03 12:39:56 +0200

    Organizing the code from layout.blade.php

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -3,14 +3,6 @@
     @unless(count($recipes) == 0)
 
     @foreach ($recipes as $recipe)
-    {{-- <h2>
-        <a href="/recipes/{{$recipe['id']}}">
-            {{ $recipe['recipe_title'] }}
-        </a>
-    </h2>
-    <p>
-        {{ $recipe['recipe_commentary'] }}
-    </p> --}}
 
     <x-recipe-card :recipe="$recipe" />
 
