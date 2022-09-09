7dc46d79190c4960e0bd4d4016ef974b633aabb7
hvtk <hvtk1968@gmail.com>
2022-09-09 16:32:23 +0200

    changed the format from the footer

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,10 +1,10 @@
-<footer class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-blue-800 text-white h-24 mt-24 opacity-90 md:justify-center"
+<footer class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-blue-800 text-white h-14 mt-24 opacity-90 md:justify-center"
       >
         <a href="/recipes/create"
-           class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
+           class="absolute top-1/3 right-10 text-white py-2 px-5 flex items-center mr-4 hover:text-blue-100"
           >
           
-            Post Recipe
+            Create a Recipe
 
         </a>
 </footer> 
