ed2ea5fc3ec3674380444d47715d0cc8979c8723
hvtk <hvtk1968@gmail.com>
2022-10-12 08:25:13 +0200

    Moved the routes from profileUser

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RecipeController;
-use App\Http\Controllers\ProfileUserController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -121,36 +119,6 @@ Route::post('/logout', [UserController::class, 'logout'])
 
 // END ALL AUTHENTICATE ROUTES
 
-// ALL PROFILE USER ROUTES
-
-//All Profiles
-Route::get('/profile', [ProfileUserController::class, 'index'])
-       ->middleware('auth'); 
-
-//Show Profile Create Form
-Route::get('/profile/create', [ProfileUserController::class, 'create'])
-       ->middleware('auth');       
-       
-//Store Profile Data
-Route::post('/profile', [ProfileUserController::class, 'store'])
-       ->middleware('auth');
-
-//Show Edit Profile Form
-Route::get('/profile/{profile}/edit', [ProfileUserController::class, 'edit'])
-       ->middleware('auth');
-
-//Update Profile
-Route::put('/profile/{profile}', [ProfileUserController::class, 'update'])
-       ->middleware('auth');
-
-//Delete Profile
-Route::delete('/profile/{profileUser}', [ProfileUserController::class, 'destroy'])
-       ->middleware('auth');
-
-//Show Profile
-Route::get('/profile/{profileUser}', [ProfileUserController::class, 'show'])
-       ->middleware('auth');
-//END ALL PROFILE USER ROUTES
 
 
 
