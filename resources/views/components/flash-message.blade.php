15793af80e268e3444a6a9291348630e80eb4009
hvtk <hvtk1968@gmail.com>
2022-12-26 13:10:38 +0100

    Changed the message css.

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,8 +1,11 @@
+<!-- Custom styles for this template -->
+<link href="{{ url('/css/flashMessage.css') }}" rel="stylesheet"/>
+
 @if(session()->has('message'))
     <div x-data="{show: true}"
          x-init="setTimeout(() => show = false, 3000)"
          x-show="show"
-         class="text-center text-black alert alert-info ms-250"
+         class="body text-center text-black alert alert-info"
         >
         <p> 
             {{ session('message') }}
