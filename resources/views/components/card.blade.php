757958524d325e95b260ba14431ec9281fa129cb
hvtk <hvtk1968@gmail.com>
2023-01-22 16:02:15 +0100

    Finished the code for the sign-in page.

--- resources/views/components/card.blade.php
+++
@@ -1,5 +0,0 @@
-<div {{ $attributes->merge(['class' => 'w-full overflow-hidden rounded-lg shadow-xs']) }}>
-
-    {{ $slot }}
-
-</div>
--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,37 +1,23 @@
 <x-layout>
 
-    <!-- Custom styles for this template -->
-    <link href="{{ url('/css/login.css') }}" rel="stylesheet"/>
-    
-    <!-- Group of symbols for this template -->
-    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-        <symbol id="book" viewBox="0 0 16 16">
-            <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
-        </symbol>
-    </svg>
+    <div class="row m-3">
 
-    <main  class="d-flex justify-content-center align-items-center col align-self-center py-3">
+        <div class="col-11 offset-1 bg-secondary min-vh-100"
+            >
 
-        <div class="body-login">
-
-            <div class="container text-center">
+            <x-card-center class="row min-vh-100">
                 
-                <div class="form-control w-50 m-auto">
+                <div class="col-2">
                     <form method="POST"
                           action="/users/authenticate"
                         >
                         @csrf
 
-                        <svg class="bi pe-none me-2" 
-                             width="30" 
-                             height="24"
-                            >
-                            <use xlink:href="#book"/>
-                        </svg>
+                        <x-book-image-card />
 
-                        <h1 class="h3 mb-3 fw-normal">
+                        <x-card-center class="h2">
                             Please sign in
-                        </h1>
+                        </x-card-center>
 
                         <div class="form-control">
 
@@ -75,19 +61,23 @@
 
                         </div>
 
-                        <div class="checkbox mb-3">
+                        <x-card-center class="checkbox mb-3">
                             <label>
-                            <input type="checkbox" value="remember-me"> Remember me
+                                <input type="checkbox" value="remember-me"> Remember me
                             </label>
-                        </div>
+                        </x-card-center>
 
-                        <button class="btn btn-lg btn-primary" 
-                                type="submit"
-                            >
-                                    Sign in
-                        </button>
+                        <x-card-center>
 
-                        <div class="mt-8">
+                            <button class="btn btn-lg btn-primary" 
+                                    type="submit"
+                                >
+                                        Sign in
+                            </button>
+
+                        </x-card-center>
+
+                        <x-card-center class="mt-2">
                             <p>
                                 Don't have an account?
                                 <a class="text-primaryblue-600"
@@ -95,17 +85,22 @@
                                     Register
                                 </a>
                             </p>
-                        </div>
+                        </x-card-center>
 
-                        <p class="mt-5 mb-3 text-muted">&copy; 2022</p>
+                        <x-card-center> 
+                              
+                            <p> &copy; 2023 </p> 
+                        
+                        </x-card-center>
 
                     </form>
+
                 </div>
 
-            </div>
+            </x-card-center>
 
         </div>
 
-    </main>
+    </div>
 
 </x-layout>
